4e152a878cf65809b7bec3d9c05a4167adb58d69
Pauline-Chan <162075495+Pauline-Chan@users.noreply.github.com>
2024-04-04 12:30:17 +0800

    Add files via upload

---
+++ riddle.js
@@ -0,0 +1,5 @@
+function h1() { document.getElementById('dip1').hidden = true } 
+function s1() { document.getElementById('dip1').hidden = false }
+
+function h2() { document.getElementById('dip2').hidden = true } 
+function s2() { document.getElementById('dip2').hidden = false }
